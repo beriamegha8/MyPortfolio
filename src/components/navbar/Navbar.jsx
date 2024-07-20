080d1e15dbc6166181a28a5d8a47b3ed5de77460
Megha Beria <beriamegha8@gmail.com>
2024-07-20 22:00:19 +0530

    Added images and working navbar element

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.scss"
+
+export const Navbar = () => {
+  return (
+    <div classname="navbar">
+        {/* Sidebar */}
+        <div className="wrapper">
+            <span>Megha Beria</span>
+            <div className="social">
+                <a href="#"><img src="/instagram.png" alt=""></img></a>
+            </div>
+        </div>
+    </div>
+  )
+}
