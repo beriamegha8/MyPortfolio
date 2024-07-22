fdd35d5dc626c13db3a553e53cb686a5b4c8552e
Megha Beria <beriamegha8@gmail.com>
2024-07-23 01:54:37 +0530

    Modified navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import "./navbar.scss"
 
 export const Navbar = () => {
   return (
-    <div classname="navbar">
+    <div className="navbar">
         {/* Sidebar */}
         <div className="wrapper">
             <span>Megha Beria</span>
             <div className="social">
-                <a href="#"><img src="/instagram.png" alt=""></img></a>
+                <a href="https://www.instagram.com/"><img src="/instagram.png" alt="" className= "logo"></img></a>
+                <a href="https://www.linkedin.com/in/megha-beria-209b75251/"><img src="/linkedin.png" alt="" className= "logo"></img></a>
+                <a href="https://github.com/beriamegha8"><img src="/github.png" alt="" className= "logo"></img></a>
             </div>
         </div>
     </div>
