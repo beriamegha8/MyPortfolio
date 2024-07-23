19ff7b66c14c8ea8d35f4b8e013376c4361088bc
Megha Beria <beriamegha8@gmail.com>
2024-07-24 00:00:16 +0530

    Added Framer box

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import "./app.scss"
+import React from "react";
+import "./app.scss";
 import { Navbar } from "./components/navbar/Navbar";
+import { Test } from "./Test";
 
 const App = () => {
-  return <div>
-    <section>
-      <Navbar/>
-    </section>
-    <section>Parallax</section>
-    <section>Services</section>
-    <section>Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section>Contact</section>
-  </div>;
+  return (
+    <div>
+      {/* <section>
+        <Navbar/>
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section> */}
+      <Test />
+    </div>
+  );
 };
 
 export default App;
